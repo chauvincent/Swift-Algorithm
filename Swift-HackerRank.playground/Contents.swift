714ed3a1159083afb36c6e2eee2daad428e52c63
Vincent Chau <vichau@ucdavis.edu>
2016-09-30 00:50:53 -0700

    started on making anagrams problem

--- Swift-HackerRank.playground/Contents.swift
+++ Swift-HackerRank.playground/Contents.swift
@@ -9,21 +9,24 @@ import UIKit
     Sample Input:               Output:
     5 4                         5 1 2 3 4
     1 2 3 4 5
-*/
 
-/*  My Thoughts:
+
+    My Thoughts:
     Arr length = 6,  if d = 2 then [..d...d+1..d+2]   the tail is equal to (arr.length - d)
-                    the head is for [d-n.. d-2.. d-1 .. d]  which is d..0 d--
+                    the head is for [d-n.. d-2.. d-1 .. d]  which is d..0
     1 2 3 4 5  d = 2
+    (tail) starts at d...d+1..d+2..d+n
     d[0] = d[2]    3
     d[1] = d[3]    4
     d[2] = d[4]    5
- 
+    (head) starts at 0..d
     d[0] = d[3]    1
     d[1] = d[4]    2
 */
 
-func leftRotate(arr: [Int], d: Int) -> [Int]
+/* My Solution O(N):
+
+ func leftRotate(arr: [Int], d: Int) -> [Int]
 {
     let length = arr.count - 1
     var rotated = [Int](repeating: 0, count: arr.count)
@@ -31,6 +34,7 @@ func leftRotate(arr: [Int], d: Int) -> [Int]
     
     for (index, element) in arr.enumerated()
     {
+        
         if (index >= (length - d))
         {
             rotated[startIndex] = element
@@ -46,5 +50,38 @@ func leftRotate(arr: [Int], d: Int) -> [Int]
 }
 
 let arr = [1,2,3,4,5]
+
 leftRotate(arr: arr, d: 2)
+
+*/
+
+/* Strings: Making Anagrams
+    Given two large strings, a and b which may or may not be the same length. Determine the minimum number of character  deletions required to make a and b anagrams. Any characters can be deleted from either of the strings
+ 
+    Input: cde      Output:  4
+           abc
+ 
+    Example:
+        - remove d and e from a to get c
+        - remove a and b from b to get c
+        - must delete 4 characters total to make an anagram, so output 4 on a new line
+ 
+ 
+    My Thoughts:
+    First... there would be three cases
+    
+    1. string a and b are same length
+    2. a is longer
+    3. b is longer
+ 
+    arr[25] ascii for alphabet as key and count as value
+    arr2[25] ascii for second string
+ 
+ 
+ 
+*/
+
+
+
+
 
